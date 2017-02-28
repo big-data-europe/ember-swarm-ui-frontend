9ef218a35d49bda4c85068e7ba4c3fe2d3e0cd16
Nóra Szepes <nora.szepes@tenforce.com>
2017-02-28 10:56:15 +0100

    clear up jshint errors and warnings in models - unused imports

--- app/models/pipeline-instance.js
+++ app/models/pipeline-instance.js
@@ -1,9 +1,9 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import Ember from 'ember';
 import HasSerialActions from '../mixins/has-serial-actions';
 
-export default DS.Model.extend({
+export default DS.Model.extend(HasSerialActions, {
   title: attr('string'),
   icon: attr('string'),
   mdlIcon: attr('string'),
--- app/models/service.js
+++ app/models/service.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
-import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: attr('string'),
