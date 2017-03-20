976802395102d7cf3fb0d19217c6cf52608d79f8
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-20 11:14:54 +0100

    added calculation for number of pipelines to the repository model level

--- app/models/repository.js
+++ app/models/repository.js
@@ -5,5 +5,10 @@ export default DS.Model.extend({
   location: attr('string'),
   title: attr('string'),
   icon: attr('string'),
-  pipelineInstances: DS.hasMany('pipeline-instance')
+  pipelineInstances: DS.hasMany('pipeline-instance'),
+
+  numberOfPipelines: Ember.computed('pipelineInstances','pipelineInstances.[]', function(){
+    // TODO: this won't work like this as long as we don't do a proper ember.createRecord for a pipeline
+    return this.get('pipelineInstances.length');
+  })
 });
