c072886481bf7386c4ca7ff0ba2b1c82aca33524
Nóra Szepes <nora.szepes@tenforce.com>
2017-05-05 10:50:37 +0200

    jshint fix

--- app/models/repository.js
+++ app/models/repository.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
@@ -8,7 +9,6 @@ export default DS.Model.extend({
   pipelineInstances: DS.hasMany('pipeline-instance'),
 
   numberOfPipelines: Ember.computed('pipelineInstances','pipelineInstances.[]', function(){
-    // TODO: this won't work like this as long as we don't do a proper ember.createRecord for a pipeline
     return this.get('pipelineInstances.length');
   })
 });
