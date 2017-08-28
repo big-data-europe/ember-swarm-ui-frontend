6c9431016a2c29370920ced791858af2b9d4124f
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-28 13:52:24 +0200

    updated to ember version 2.14.2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,11 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
     sassOptions: {
       includePaths: [
