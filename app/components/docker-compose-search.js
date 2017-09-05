7ffad0e70e0cb5e5b28184d43a796402e7ecc2de
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-05 10:09:18 +0200

    docker search works, still need redesigning

---
+++ app/components/docker-compose-search.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+import {
+  task,
+  timeout
+} from 'ember-concurrency';
+import env from '../config/environment';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  classNames: ['docker-compose-search'],
+  dockerComposeList: task(function*(term) {
+    // if (Ember.isBlank(term)) {
+    //   return this.get('model');
+    // }
+
+    // Pause here for DEBOUNCE_MS milliseconds. Because this
+    // task is `restartable`, if the user starts typing again,
+    // the current search will be canceled at this point and
+    // start over from the beginning. This is the
+    // ember-concurrency way of debouncing a task.
+    yield timeout(env.searchDebounceMiliseconds);
+
+    return yield this.filterComposeFiles(term);
+  }).on('init').restartable(),
+
+  filterComposeFiles: function(searchValue){
+    var params = {
+      sort: 'title'
+    };
+    if (searchValue.length > 0) {
+      params.filter = {
+        title: searchValue
+      };
+    } else {
+      params.page = {
+        size: env.defaultNumberOfContainers,
+        number: 0
+      };
+    }
+
+    return this.get('store').query('docker-compose', params);
+  }
+});
